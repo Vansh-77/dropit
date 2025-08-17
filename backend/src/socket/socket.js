f8cfc2d95126157c9b4beefd4a930e3bc49c43eb
Vansh-77 <Vanshbhardwaj733@gmail.com>
2025-08-17 19:37:55 +0530

    d

--- backend/src/socket/socket.js
+++ backend/src/socket/socket.js
@@ -4,25 +4,36 @@ const rooms = new Map();
 
 export default function (io) {
     io.on('connection', socket => {
+        socket.on("roomExists", ({ roomName }) => {
+            if (rooms.has(roomName)) {
+                socket.emit("roomExists", true);
+            }
+        });
         socket.on('joinRoom', ({ roomName, password }) => {
             try {
                 const room = rooms.get(roomName);
                 if (!room) {
-                    socket.emit('error', 'room not found');
+                    socket.emit('error', 'Room not found');
                     return;
                 }
                 if (room.password !== password) {
                     socket.emit('error', 'Invalid password');
                     return;
                 }
+                const existingMembers = [...room.members];
+                socket.emit("initPeers", existingMembers);
+
                 room.members.add(socket.id);
+                socket.roomId = roomName;
+
                 room.members.forEach(peerId => {
                     if (peerId !== socket.id) {
                         io.to(peerId).emit('newMember', socket.id);
                     }
                 });
-                socket.roomId = roomName;
+                
                 socket.emit('success', `Joined room: ${roomName}`);
+                console.log(rooms);
             } catch (error) {
                 console.log(error);
                 socket.emit('InternalError', 'Internal server error');
@@ -36,7 +47,7 @@ export default function (io) {
                     socket.emit('error', 'Room Name already exists');
                     return;
                 }
-                rooms.set(roomName , {
+                rooms.set(roomName, {
                     members: new Set([socket.id]),
                     password: password,
                 });
