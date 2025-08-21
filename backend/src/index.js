474890f7819a1447e250b45fd00eb3e6174f6d38
Vansh-77 <Vanshbhardwaj733@gmail.com>
2025-08-21 20:02:17 +0530

    d

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,24 +1,24 @@
 import express from 'express';
 import cors from "cors";
-import {createServer}from "http";
-import {Server} from "socket.io";
+import { createServer } from "http";
+import { Server } from "socket.io";
 import socket from './socket/socket.js';
 import "dotenv/config";
 
 const app = express();
 
 const server = createServer(app);
-const io = new Server(server,{cors:{origin:"*"}});
+const io = new Server(server, { cors: { origin: "*", methods: ["GET", "POST"] } });
 
 app.use(express.json());
 app.use(cors());
 
-app.use('/',(req ,res)=>{
+app.use('/', (req, res) => {
     res.send("dropit backend is running");
 });
 
 socket(io);
 
-server.listen(process.env.PORT || 3000,()=>{
+server.listen(process.env.PORT || 3000, () => {
     console.log(`Server is running on port ${process.env.PORT || 3000}`);
 });
--- frontend/socket/socket.js
+++ frontend/socket/socket.js
@@ -4,7 +4,6 @@ import {io} from "socket.io-client";
 const socket = io(import.meta.env.VITE_SOCKET_URL,{
     autoConnect:true,
     transports:["websocket"],
-    secure:true,
     withCredentials:true,
 });
 
